451dc6e3546424619078135aa653a4460d8003a7
Edmund Kotei <edmundkotei@gmail.com>
2020-03-12 10:09:43 -0400

    aye babay

---
+++ src/main/java/testpack/Main.java
@@ -0,0 +1,22 @@
+package testpack;
+
+public class Main {
+    public static void main(String[] args) {
+        byte x = 127;
+        System.out.println(x);
+
+        float myFloat = 3.12f;
+        System.out.println(myFloat);
+
+        for(int p = 0; p < 10; p++){
+            System.out.println(p);
+
+            for (int f = 1; f < 6; f++){
+                System.out.println(f);
+            }
+        }
+
+        C:\Users\Edmun\Desktop\Java Practice\Edmun Kotei
+
+    }
+}
